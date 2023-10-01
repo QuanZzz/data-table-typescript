d3a42e0432ab422cb4cd3174a0ddf36cace05dff
QuanZzz <zhouquaneric@gmail.com>
2023-09-30 21:56:12 -0400

    date format tweaks

--- src/routes/CustomersTable.tsx
+++ src/routes/CustomersTable.tsx
@@ -2,12 +2,33 @@ import DataTable from "../components/DataTable";
 import { usersInfo, usersInfoColumns } from "../utils/constants/usersInfo";
 import { CUSTOMERS_TABLE_ROWS_NUMBERS } from "../utils/constants/constants";
 
+interface UserInfo {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  created_at: string;
+}
+
 export default function CustomersTable() {
+  const formattedUsersInfo: UserInfo[] = usersInfo.map((user) => {
+    const createdAt = new Date(user.created_at);
+    const year = createdAt.getFullYear();
+    const month = String(createdAt.getMonth() + 1).padStart(2, "0");
+    const day = String(createdAt.getDate()).padStart(2, "0");
+    const hours = String(createdAt.getHours()).padStart(2, "0");
+    const minutes = String(createdAt.getMinutes()).padStart(2, "0");
+    const seconds = String(createdAt.getSeconds()).padStart(2, "0");
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return { ...user, created_at: formattedDate };
+  });
+
   return(
     <div>
       <DataTable
         header="Customers Information"
-        initialData={usersInfo}
+        initialData={formattedUsersInfo}
         columns={usersInfoColumns}
         rowsSizes={CUSTOMERS_TABLE_ROWS_NUMBERS}
       />
